92f3c73e01bb2e2bf68907035dac247188bfbb89
Nqobani Nhlengethwa <nqobani.nhlengethwa@impact.com>
2023-06-17 15:00:15 +0200

    project configuration

---
+++ src/main/java/com/messenger/messengerApp/MessengerAppApplication.java
@@ -0,0 +1,14 @@
+package com.messenger.messengerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MessengerAppApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerAppApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/messenger/messengerApp/MessengerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messenger.messengerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
